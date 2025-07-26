673548dd6cabd0508147b2b2fc11e032bed56404
arturogood17 <arturo_good17@hotmail.com>
2025-07-26 23:18:31 +0200

    This is the army algo. I based mine on dattebayo's algo. It needs some debugging.

--- shoe_lacing_algos.py
+++ shoe_lacing_algos.py
@@ -15,13 +15,33 @@ def crisscross(text):
         else:
             raise ValueError(f'Error: "{chr(ascii_value)}" is not supported \nUppercase letters (A-Z) are supported only')
 
-    return output
+    return "".join(output)
 
 
+def army(text):
+    i = 0                                   #itereator
+                                            #list of input text
+    output = []                             #output var
+
+    while i < len(text):                    #runs till all characters are converted
+        ascii_value = ord(text[i])  #ascii value of letter
+        if ascii_value in range(ord('A'), ord('Z')): #check if the char is within A-Z character limit
+            #Here I am checking if the direction towards the next letter is downwards or diagonal
+            #If % 2 == 0, it could mean 2 steps ahead or 3.
+            if ascii_value % 4 == 0: 
+                ascii_value +=1
+            else:
+                if ascii_value % 2 != 0 and (ascii_value + 1) % 4 == 0:
+                    ascii_value +=3
+                else:
+                    ascii_value +=2
+            output.append(chr(ascii_value))     #appending after converting back to chr
+            i += 1
+        else:
+            raise ValueError(f'Error: "{chr(ascii_value)}" is not supported \nUppercase letters (A-Z) are supported only')
+
+    return "".join(output)
+            
 def straight_european(secret):
-    print("straight_european algo would go here")
-    print()
-
-def army(secret):
     print("Army algo would go here")
     print()
