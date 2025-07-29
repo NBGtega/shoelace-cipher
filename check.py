65ae8838e5e47245ca75f62fb867bd090cb85125
dattebayo <fkgame061@gmail.com>
2025-07-29 13:25:30 +0500

    +key encryption/decryption +fixed some bugs +refactored a lil bit +cleaned help output

--- check.py
+++ check.py
@@ -1,5 +1,5 @@
 from shoe_lacing_algos import crisscross, army, straight_european
-from helper_func import check
+from helper_func import check, xor_encrypt_hex, xor_decrypt_hex
 
 text_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 text_lower = text_upper.lower()
@@ -43,8 +43,22 @@ def check_str_eur():
     print("Checking straight_european algorithm...")
     check(expected_lower, "".join(actual_lower), expected_upper, "".join(actual_upper))
 
+def check_xor():
+    actual_upper = xor_decrypt_hex(xor_encrypt_hex(text_upper, "magic value kira kira"), "magic value kira kira")
+    actual_lower = xor_decrypt_hex(xor_encrypt_hex(text_lower, "magic value kira kira"), "magic value kira kira")
+    print("Checking xor...")
 
+    if actual_upper != text_upper:
+        print("Failed upper case test")
+    elif actual_lower != text_lower:
+        print("Failed lower case test")
+    else:
+        print("Passed upper case test\nPassed lower case")
 
 check_criss_cross()
-check_army() 
+print()
+check_army()
+print()
 check_str_eur()
+print()
+check_xor()
